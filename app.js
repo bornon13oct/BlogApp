c5ae8fc522330b48f7f1e32ccde2cfcedbc5b380
bornon13oct <bornon13oct@gmail.com>
2018-04-07 16:18:06 +0000

    added landing page

---
+++ app.js
@@ -0,0 +1,44 @@
+var express                 = require("express"),
+    app                     = express(),
+    bodyParser              = require("body-parser"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    flash                   = require("connect-flash"),
+    methodOverride          = require("method-override"),
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    User                    = require("./models/user"),
+    LocalStrategy           = require("passport-local");
+    
+mongoose.connect("mongodb://skay13oct:bornon13oct@ds237979.mlab.com:37979/cashpositive");
+
+app.use(flash());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(require("express-session")({
+    secret: "I love nodejs",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error     = req.flash("error");
+    res.locals.success     = req.flash("success");
+    next();
+});
+app.use(methodOverride("_method"));
+
+app.get("/", function (req, res) {
+    res.render("landing");
+});
+
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("server has started");
+});
