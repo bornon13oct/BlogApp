530a721e473d34ffddf2d8cb391188922e4620ac
bornon13oct <bornon13oct@gmail.com>
2018-04-07 16:28:15 +0000

    added isLoggedIn function

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ app.post("/register", function(req, res) {
     });
 });
 
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You need to be logged in to that");
+    res.redirect("/login");
+}
+
 app.listen(process.env.PORT, process.env.IP, function () {
     console.log("server has started");
 });
